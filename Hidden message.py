74014605afd083eeb10e49a8b99c945acbe69a14
Konstantin <vasilevki@gmail.com>
2022-04-18 13:02:10 +0300

    Test

---
+++ Hidden message.py
@@ -0,0 +1,14 @@
+# 3 - Е
+# 4 – Ч
+# 6 – Б
+# 8 – В
+# 9 – Д
+# 0 - О
+message = 'Мн3 4у9ится, 4т0 кт0-т0 4итает с006щения!!! М0ж3т я 8се при9умала, н0 6у9ь на43ку, 0к?'
+message = message.replace("3", "е")
+message = message.replace("4", "ч")
+message = message.replace("6", "б")
+message = message.replace("8", "в")
+message = message.replace("9", "д")
+message = message.replace("0", "о")
+print(message)
