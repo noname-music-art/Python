b7bf8af16cf214cefca4d6539ae47ca5da1d1d1c
Konstantin <vasilevki@gmail.com>
2022-04-18 12:34:13 +0300

    Test

--- Strong pwd.py
+++ Strong pwd.py
@@ -6,11 +6,16 @@ bad_pass = False
 if len(login) <= 3:
     print("Логин должен содержать больше 3 символов.")
     bad_pass = True
+
 if len(pwd) <= 8:
     print("Пароль должен содержать больше 8 символов.")
     bad_pass = True
+
 if pwd.isalpha():
     print("Пароль должен содержать хотя бы одну цифру.")
     bad_pass = True
+
 if not bad_pass:
     print("Это хорошие логин и пароль!")
+
+'''Test'''
