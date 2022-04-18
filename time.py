6a42e8cbf116f0668d6482c64b36d391e2430487
Konstantin <vasilevki@gmail.com>
2022-04-18 16:04:20 +0300

    Test

--- time.py
+++ time.py
@@ -1,12 +1,15 @@
 user_input = input()
-len(user_input)
+
 hours = 0
 minutes = 0
+
 for char in user_input:
     if char == '*':
         hours += 1
     elif char == ".":
         minutes += 15
-    else:
-        print("wrong element")
+
+hours = hours + minutes // 60
+minutes = minutes % 60
+
 print(f"{hours}:{minutes}")
