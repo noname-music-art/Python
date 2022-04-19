f954c5f9b2fab34a5da771109d7d92db8491d1ce
Konstantin <vasilevki@gmail.com>
2022-04-19 10:23:09 +0300

    Test

--- additional_hw/Hidden message.py
+++ additional_hw/Hidden message.py
@@ -1,14 +1,7 @@
-# 3 - Е
-# 4 – Ч
-# 6 – Б
-# 8 – В
-# 9 – Д
-# 0 - О
+dict_to_replace = {"3": "Е", "4": "Ч", "6": "Б", "8": "В", "9": "Д", "0": "О"}
 message = 'Мн3 4у9ится, 4т0 кт0-т0 4итает с006щения!!! М0ж3т я 8се при9умала, н0 6у9ь на43ку, 0к?'
-message = message.replace("3", "е")
-message = message.replace("4", "ч")
-message = message.replace("6", "б")
-message = message.replace("8", "в")
-message = message.replace("9", "д")
-message = message.replace("0", "о")
+
+for k, v in dict_to_replace.items():
+    message = message.replace(k, v)
+
 print(message)
