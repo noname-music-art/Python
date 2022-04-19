864b4a0cd24f3a8ef852784505097d79a2e5281d
Konstantin <vasilevki@gmail.com>
2022-04-19 16:29:24 +0300

    Test

--- additional_hw/cities.py
+++ additional_hw/cities.py
@@ -1,11 +1,23 @@
-cities = input()
+cities = ("архангельск - киров - вологда - адлер - рим - минск - киров")
 cities = cities.replace(" - ", " ")
 cities = cities.split(" ")
 
 flag = False
+last_char = None
+first = True
 
 for city in cities:
     if cities.count(city) > 1:
         flag = True
-    print(city[0])
-print(flag)
+
+    if (last_char != city[0]) and not first:
+        flag = True
+
+    print(city[0], last_char)
+    last_char = city[-1]
+    first = False
+
+if flag:
+    print(f"Правила нарушены")
+else:
+    print(f"Правила не нарушены")
