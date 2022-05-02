b3496c77a9efa8fd1ff87f0f49f1925cf3fbeac7
Konstantin <vasilevki@gmail.com>
2022-05-02 13:38:43 +0300

    Rename

--- additional_hw/hw_5/01_dot_count_func.py
+++ additional_hw/hw_5/01_dot_count_func.py
@@ -1,11 +1,11 @@
 def pixel_count(x_res, y_res, density=1):
     """
-    Function calculate ammount of pixel.
+    Function calculate amount of pixel.
 
     :param x_res: number of dots in width
     :param y_res: number of dots in height
     :param density: points density
-    :return: ammount of pixel
+    :return: amount of pixel
     """
 
     return x_res*y_res*density
