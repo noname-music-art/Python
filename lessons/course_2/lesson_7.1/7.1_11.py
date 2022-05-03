990625c263e7b528f5685c36c19145a112c35047
Konstantin <vasilevki@gmail.com>
2022-05-03 13:46:01 +0300

    Rename

---
+++ lessons/course_2/lesson_7.1/7.1_11.py
@@ -0,0 +1,8 @@
+order = [
+    {"skolko": 5.0, "poroda": "тунец", "sred_razmer": 300},
+    {"skolko": 15.0, "poroda": "окунь", "sred_razmer": 250},
+    {"skolko": 20.0, "poroda": "щука", "sred_razmer": 460},
+]
+order_converted = []
+
+for item in order:
