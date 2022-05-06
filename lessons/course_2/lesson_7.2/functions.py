34eb6b475e092085e048562539cf954455a4a92d
Konstantin <vasilevki@gmail.com>
2022-05-06 16:30:16 +0300

    Rename

--- lessons/course_2/lesson_7.2/functions.py
+++ lessons/course_2/lesson_7.2/functions.py
@@ -29,7 +29,9 @@ def get_student_by_pk(pk):
     """
     students = load_students()
 
-    return students[pk-1]
+    student_name = students[pk-1]['full_name']
+    student_skills = students[pk - 1]['skills']
+    return student_name, student_skills
 
 
 def get_profession_by_title(title):
@@ -39,7 +41,17 @@ def get_profession_by_title(title):
     :return:
     """
     professions = load_professions()
+
     for i in range(0, len(professions)):
         if title in professions[i]['title']:
             return professions[i]['skills']
 
+
+def check_fitness(student, profession):
+    fitness = {}
+    student_skills = set(student)
+    profession_skills = set(profession)
+    fitness['has'] = student_skills.intersection(profession_skills)
+    fitness['lacks'] = profession_skills.difference(student_skills)
+    fitness['fit_percent'] = len(fitness['has']) / len(profession_skills) * 100
+    return fitness
--- lessons/course_2/lesson_7.2/main.py
+++ lessons/course_2/lesson_7.2/main.py
@@ -1,12 +1,20 @@
-from functions import load_students
-from functions import load_professions
 from functions import get_student_by_pk
 from functions import get_profession_by_title
+from functions import check_fitness
 
 
-students = load_students()
-professions = load_professions()
+pk = int(input("Введите номер студента\n"))
 
-print(get_student_by_pk(1))
+student_name, student_skills = get_student_by_pk(pk)
 
-print(get_profession_by_title('Frontend'))
+print(f"Студент {student_name}\nЗнает: {', '.join(student_skills)}")
+
+title = input(f"Выберите специальность для оценки студента {student_name}\n")
+
+profession_skills = get_profession_by_title(title)
+
+fitness = check_fitness(student_skills, profession_skills)
+
+print(f"{int(fitness['fit_percent'])}%\n"
+      f"{student_name} знает {', '.join(fitness['has'])}\n"
+      f"{student_name} не знает {', '.join(fitness['lacks'])}")
