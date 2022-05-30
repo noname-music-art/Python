ad25bfd7d6b31a46879180328eabddf177168230
Konstantin <vasilevki@gmail.com>
2022-05-30 15:45:49 +0300

    Rename

--- lessons/course_2/course_work_2/player.py
+++ lessons/course_2/course_work_2/player.py
@@ -3,9 +3,10 @@ class Player:
     def __init__(self, user_name):
         self.user_name = user_name
         self.used_words = []
+        self.correct_words = []
 
     def correct_words(self):
-        return len(self.used_words)
+        return len(self.correct_words)
 
     def add_word(self, user_word):
         self.used_words.append(user_word)
